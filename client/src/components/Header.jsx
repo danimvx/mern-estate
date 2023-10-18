36349d0feda356a81ff73be1f23ffa6d7eb912a7
danimvx <danielmvx@gmail.com>
2023-10-18 15:10:21 -0300

    update header and make profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -27,7 +30,16 @@ function Header() {
             <Link to="/about">About</Link>
           </li>
           <li className="text-slate-700 hover:underline">
-            <Link to="/sign-in">Sign In</Link>
+            <Link to="/profile">
+              {currentUser ? (
+                <img
+                  className="rounded-full h-7 w-7 object-cover"
+                  src={currentUser.avatar}
+                />
+              ) : (
+                "Sign In"
+              )}
+            </Link>
           </li>
         </ul>
       </div>
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -27,7 +27,7 @@ function OAuth() {
       });
       const data = await res.json();
       dispatch(signInSuccess(data));
-      navigate("/");
+      navigate("/profile");
     } catch (error) {
       console.log("could not sign in with google", error);
     }
